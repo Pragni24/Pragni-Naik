ca358d9687aa8f083140ad34f9dbf8670271e155
Naik Pragni <129032980+Pragni24@users.noreply.github.com>
2023-11-29 23:23:40 +0530

    update

---
+++ OOP 6 Days Challenge/Day2/Code_Day2/src/packages/staticExample/Main.java
@@ -0,0 +1,9 @@
+package packages.staticExample;
+
+public class Main {
+    public static void main(String[] args) {
+        Human kunal = new Human(22,"Kunal",10000,false);
+        Human rahul = new Human(27,"Rahul",150000,true);
+        System.out.println(kunal.name);
+    }
+}
